c4f26f7531bc1cb00a7c9a879a95ad382ed58833
jimohf <fatima.jimoh@atrizult.com>
2026-03-11 17:51:20 +0100

    Implement header, sidebar for patient view page

---
+++ app/auth/patient/page.tsx
@@ -0,0 +1,12 @@
+import Header from '@/components/patient/Header'
+import React from 'react'
+
+const PatientDashboard = () => {
+  return (
+    <>
+    <Header />
+    </>
+  )
+}
+
+export default PatientDashboard
